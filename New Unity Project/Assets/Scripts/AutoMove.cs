e7083505f17824af83924d807e7548b2275486cd
miyasho0504 <miyashi405@gmail.com>
2016-02-09 18:13:14 +0900

    メイン開始時にカウントダウンを行うように変更 それにともない、仮置きのカウントダウンパネル用マテリアルを4つ追加

--- New Unity Project/Assets/Scripts/AutoMove.cs
+++ New Unity Project/Assets/Scripts/AutoMove.cs
@@ -8,8 +8,8 @@ public class AutoMove : MonoBehaviour {
     private float intervalTime_;
     public GameObject stair_;
     public float moveYZ_=0.2f;//どれだけ大きく移動させるか
-    private bool automove1_=false;//自動移動の1回目(大きめの移動)がなされたか 
-	
+    private bool automove1_=false;//自動移動の1回目(大きめの移動)がなされたか
+    public static bool automove_ = false;//自動移動を行うか否か
     // Use this for initialization
 	void Start () {
         setTime_ = 60.0f / bpm_;//BPMから次のジャンプまでの時間を計算する
@@ -21,26 +21,30 @@ public class AutoMove : MonoBehaviour {
         Vector3 pos;
         Vector3 scale;
         //だいたい(setTime_)秒ごとに処理を行う
-        intervalTime_ -= Time.deltaTime;
-        if (intervalTime_ <= 0.1)//0.1秒前から判定を入れる
+        if (automove_ == true)
         {
-            pos = transform.position;               //大きく移動させる前のポジションを記録
-            scale = stair_.transform.localScale;    //階段の大きさ(何倍か)を記録
-            //まずは移動させたい距離より大きめに移動させて
-            if(automove1_==false){
-                pos.y -= 1.0f*scale.y+moveYZ_;
-                pos.z -= 1.0f * scale.z + moveYZ_;
-                transform.position = pos;
-                automove1_ = true;
-            }
-            //0.1秒後に元に戻す(ただし自動移動の1回目が行われてから)
-            if (intervalTime_ <= 0.0&&automove1_==true)
+            intervalTime_ -= Time.deltaTime;
+            if (intervalTime_ <= 0.1)//0.1秒前から判定を入れる
             {
-                intervalTime_ = setTime_;
-                pos.y += moveYZ_;
-                pos.z += moveYZ_;
-                transform.position = pos;
-                automove1_ = false;
+                pos = transform.position;               //大きく移動させる前のポジションを記録
+                scale = stair_.transform.localScale;    //階段の大きさ(何倍か)を記録
+                //まずは移動させたい距離より大きめに移動させて
+                if (automove1_ == false)
+                {
+                    pos.y -= 1.0f * scale.y + moveYZ_;
+                    pos.z -= 1.0f * scale.z + moveYZ_;
+                    transform.position = pos;
+                    automove1_ = true;
+                }
+                //0.1秒後に元に戻す(ただし自動移動の1回目が行われてから)
+                if (intervalTime_ <= 0.0 && automove1_ == true)
+                {
+                    intervalTime_ = setTime_;
+                    pos.y += moveYZ_;
+                    pos.z += moveYZ_;
+                    transform.position = pos;
+                    automove1_ = false;
+                }
             }
         }
 	}
---
+++ New Unity Project/Assets/Scripts/MainGame.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGame : MonoBehaviour {
+    public float startTime_ = 5.0f;//何秒後にスタートさせるか
+    public AudioClip bgm_;
+    private AudioSource audioSource;
+
+    public GameObject Count1_;
+    public GameObject Count2_;
+    public GameObject Count3_;
+    public GameObject CountStart_;
+
+    private bool panel_created_ = false;//パネルが生成されたか
+    private bool panelS_deleted_ = false;//スタートパネルが削除されたか
+    private bool panel1_deleted_ = false;//１パネルが削除されたか
+    private bool panel2_deleted_ = false;//２パネルが削除されたか
+    private bool panel3_deleted_ = false;//３パネルが削除されたか
+
+    bool gameEnd_ = false;//ゲームクリアもしくはゲームオーバーになったか
+
+	// Use this for initialization
+	void Start () {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = bgm_;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (AutoMove.automove_ == false)
+        {
+            startTime_ -= Time.deltaTime;
+            //カウントダウン
+            if(startTime_<=3.0f&&panel_created_==false){
+                Instantiate(CountStart_);
+                Instantiate(Count1_);
+                Instantiate(Count2_);
+                Instantiate(Count3_);
+                panel_created_ = true;
+            }
+            if(startTime_<=2.0f&&panel3_deleted_==false){
+                GameObject panel3=GameObject.Find("CountPanel3(Clone)");
+                GameObject.Destroy(panel3);
+                panel3_deleted_ = true;
+            }
+            if (startTime_ <= 1.0f && panel2_deleted_ == false)
+            {
+                GameObject panel2 = GameObject.Find("CountPanel2(Clone)");
+                GameObject.Destroy(panel2);
+                panel2_deleted_ = true;
+            }
+            if (startTime_ <= 0.0f && panel1_deleted_ == false)
+            {
+                GameObject panel1 = GameObject.Find("CountPanel1(Clone)");
+                GameObject.Destroy(panel1);
+                panel1_deleted_ = true;
+                AutoMove.automove_ = true;
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            //ゲームが終了したら
+            if (gameEnd_ == true) { }   
+        }
+	}
+}
